a5c3104f1ce28d16b12920bff0c915417ecf7e12
josesanchez10 <shanchezshanchez10@gmail.com>
2022-06-11 14:52:14 -0600

    seeder

--- database/seeders/AdministratorSeeder.php
+++ database/seeders/AdministratorSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Administrator;
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +16,36 @@ class AdministratorSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $admins = [
+            [
+                'administrator_name' => 'Guillermo Hernandez',
+                'dui' => '01021117-2',
+                'phone' => '22222222',
+                'user_id' => '1'
+            ],
+            [
+                'administrator_name' => 'Giovanni Tzec',
+                'dui' => '03214678-0',
+                'phone' => '22222222',
+                'user_id' => '1'
+            ],
+            [
+                'administrator_name' => 'Jose Sanchez',
+                'dui' => '05267670-3',
+                'phone' => '77777777',
+                'user_id' => '1'
+            ],
+            [
+                'administrator_name' => 'administrador',
+                'dui' => '00000234-0',
+                'phone' => '22222222',
+                'user_id' => '1'
+            ],
+            
+        ];
+
+        foreach ($admins as $admin) {
+            $admin = Administrator::create($admin);
+        }
     }
 }
--- database/seeders/SubsidiarySeeder.php
+++ database/seeders/SubsidiarySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Subsidiary;
 
 class SubsidiarySeeder extends Seeder
 {
@@ -14,6 +15,24 @@ class SubsidiarySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $subsidiary = [
+            [
+                'subsidiary_name' => 'Guillermo Hernandez'
+            ],
+            [
+                'subsidiary_name' => 'Giovanni Tzec'
+            ],
+            [
+                'subsidiary_name' => 'Jose Sanchez'
+            ],
+            [
+                'subsidiary_name' => 'administrador'
+            ],
+            
+        ];
+
+        foreach ($subsidiary as $subs) {
+            $subs = Subsidiary::create($subs);
+        }
     }
 }
